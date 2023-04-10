c7b514309c179c70982cfd35258fe943b979fa89
ezzeldem <ezzelden06@gmail.com>
2023-04-10 23:07:34 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-$(document).ready(function(){
-    if ($("html").attr("dir") == "rtl") {
-        var dir_lang = true;
-      } else {
-        var dir_lang = false;
-      }
-  
+$(document).ready(function () {
+  if ($("html").attr("dir") == "rtl") {
+    var dir_lang = true;
+  } else {
+    var dir_lang = false;
+  }
+
+  $(".event-slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    rtl: dir_lang,
+    dots: false,
+    adaptiveHeight: true,
+    autoplaySpeed: 1700,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 772,
+        settings: {
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
